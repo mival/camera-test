f1a10dcf24fd897fac2553b072ecd4fe83773a77
mival <jan.vonavka@gmail.com>
2019-06-17 10:12:23 +0200

    WIP

--- www/js/index.js
+++ www/js/index.js
@@ -1,118 +1,165 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 
 function createNewFileEntry(imgUri) {
-    window.resolveLocalFileSystemURL(cordova.file.cacheDirectory, function success(dirEntry) {
+  window.resolveLocalFileSystemURL(cordova.file.cacheDirectory, function success(dirEntry) {
 
-        // JPEG file
-        dirEntry.getFile("tempFile.jpeg", { create: true, exclusive: false }, function (fileEntry) {
+      // JPEG file
+      dirEntry.getFile("tempFile.jpeg", { create: true, exclusive: false }, function (fileEntry) {
 
-            // Do something with it, like write to it, upload it, etc.
-            // writeFile(fileEntry, imgUri);
-            console.log("got file: " + fileEntry.fullPath);
-            // displayFileData(fileEntry.fullPath, "File copied to");
+          // Do something with it, like write to it, upload it, etc.
+          // writeFile(fileEntry, imgUri);
+          console.log("got file: " + fileEntry.fullPath);
+          // displayFileData(fileEntry.fullPath, "File copied to");
 
-        }, console.error);
+      }, console.error);
 
-    }, console.error);
+  }, console.error);
 }
 
 function openCamera() {
+  alert(JSON.stringify(window.QRScanner));
 
-    var srcType = Camera.PictureSourceType.CAMERA;
-    var options = setOptions(srcType);
-    var func = createNewFileEntry;
+  var srcType = Camera.PictureSourceType.CAMERA;
+  var options = setOptions(srcType);
+  var func = createNewFileEntry;
 
-    navigator.camera.getPicture(function cameraSuccess(imageUri) {
+  navigator.camera.getPicture(function cameraSuccess(imageUri) {
 
-        displayImage(imageUri);
-        // You may choose to copy the picture, save it somewhere, or upload.
-        func(imageUri);
+      displayImage(imageUri);
+      // You may choose to copy the picture, save it somewhere, or upload.
+      func(imageUri);
 
-    }, function cameraError(error) {
-        console.debug("Unable to obtain picture: " + error, "app");
+  }, function cameraError(error) {
+      console.debug("Unable to obtain picture: " + error, "app");
 
-    }, options);
+  }, options);
+}
+
+function displayContents(err, text){
+  if(err){
+    // an error occurred, or the scan was canceled (error code `6`)
+  } else {
+    // The scan completed, display the contents of the QR code:
+    alert(text);
+  }
+}
+
+function qrCodeCamera() {
+  // Start a scan. Scanning will continue until something is detected or
+// `QRScanner.cancelScan()` is called.
+QRScanner.scan(displayContents);
+
+// Make the webview transparent so the video preview is visible behind it.
+QRScanner.show();
+// Be sure to make any opaque HTML elements transparent here to avoid
+// covering the video.
+}
+
+function qrCodeRead() {
+  cordova.plugins.barcodeScanner.scan(
+    function (result) {
+      navigator.notification.alert("We got a barcode\n" +
+              "Result: " + result.text + "\n" +
+              "Format: " + result.format + "\n" +
+              "Cancelled: " + result.cancelled);
+    },
+    function (error) {
+      navigator.notification.alert("Scanning failed: " + error);
+    },
+    {
+        preferFrontCamera : true, // iOS and Android
+        showFlipCameraButton : true, // iOS and Android
+        showTorchButton : true, // iOS and Android
+        torchOn: true, // Android, launch with the torch switched on (if available)
+        saveHistory: true, // Android, save scan history (default false)
+        prompt : "Place a barcode inside the scan area", // Android
+        resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+        formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+        orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+        disableAnimations : true, // iOS
+        disableSuccessBeep: false // iOS and Android
+    }
+  );
 }
 
 function displayImage(imgUri) {
-
-    var elem = document.getElementById('imageFile');
-    elem.src = imgUri;
+  var elem = document.getElementById('imageFile');
+  elem.src = imgUri;
 }
 
 function setOptions(srcType) {
-    var options = {
-        // Some common settings are 20, 50, and 100
-        quality: 50,
-        destinationType: Camera.DestinationType.FILE_URI,
-        // In this app, dynamically set the picture source, Camera or photo gallery
-        sourceType: srcType,
-        encodingType: Camera.EncodingType.JPEG,
-        mediaType: Camera.MediaType.PICTURE,
-        correctOrientation: true  //Corrects Android orientation quirks
-    }
-    return options;
+  var options = {
+      // Some common settings are 20, 50, and 100
+      quality: 50,
+      destinationType: Camera.DestinationType.FILE_URI,
+      // In this app, dynamically set the picture source, Camera or photo gallery
+      sourceType: srcType,
+      encodingType: Camera.EncodingType.JPEG,
+      mediaType: Camera.MediaType.PICTURE,
+      correctOrientation: true  //Corrects Android orientation quirks
+  }
+  return options;
 }
 
 function cameraTakePicture() {
-    navigator.camera.getPicture(onSuccess, onFail, {
-       quality: 50,
-       destinationType: Camera.DestinationType.DATA_URL
-    });
+  navigator.camera.getPicture(onSuccess, onFail, {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL
+  });
 
-    function onSuccess(imageData) {
-       var image = document.getElementById('myImage');
-       image.src = "data:image/jpeg;base64," + imageData;
-    }
+  function onSuccess(imageData) {
+      var image = document.getElementById('myImage');
+      image.src = "data:image/jpeg;base64," + imageData;
+  }
 
-    function onFail(message) {
-       alert('Failed because: ' + message);
-    }
- }
+  function onFail(message) {
+      alert('Failed because: ' + message);
+  }
+}
 
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+  // Application Constructor
+  initialize: function() {
+      document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-        document.getElementById("cameraTakePicture").addEventListener ("click", cameraTakePicture);
-    },
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function() {
+      this.receivedEvent('deviceready');
+      document.getElementById("cameraTakePicture").addEventListener ("click", qrCodeRead);
+  },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+      var parentElement = document.getElementById(id);
+      var listeningElement = parentElement.querySelector('.listening');
+      var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+      listeningElement.setAttribute('style', 'display:none;');
+      receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
+      console.log('Received Event: ' + id);
+  }
 };
 
 app.initialize();
